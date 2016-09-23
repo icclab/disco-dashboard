e716d982c6695b7010c8c3359a7be222a64b3fc8
saken <s.kenzhegulov@mail.ru>
2016-09-23 11:27:25 +0200

    Title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#dashboard'
+  get 'pages/dashboard'
+
+  get 'pages/login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
