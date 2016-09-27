b9f56758342cf72cad66fe9f2b15c3c69b37ea70
Saken <s.kenzhegulov@mail.ru>
2016-09-27 17:39:15 +0200

    Some backend added: list, get

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,28 @@
 class PagesController < ApplicationController
+  @@openstack = OpenStack::Connection.create ({
+        username:   'kenz@zhaw.ch',
+        api_key:    'algorithms1',
+        auth_url:   'https://keystone.cloud.switch.ch:5000/v2.0',
+        authtenant: 'kenz@zhaw.ch',
+        region:     'ZH'
+    })
+
+  helper_method :get_image, :get_flavor
+
   def dashboard
+    @servers = @@openstack.list_servers_detail
+    @images  = @@openstack.list_images
+    @flavors = @@openstack.list_flavors
   end
 
   def login
+  end
+
+  def get_image(id)
+    @@openstack.get_image(id)
+  end
+
+  def get_flavor(id)
+    @@openstack.get_flavor(id)
   end
 end
