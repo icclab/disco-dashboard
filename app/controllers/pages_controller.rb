1fbe082ed30eda94f23cb91c35c9e435b148da58
Saken <s.kenzhegulov@mail.ru>
2016-10-10 18:06:44 +0200

    few fixes related to ajax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -62,68 +62,7 @@ class PagesController < ApplicationController
     return list
   end
 
-  # Method to create a cluster on DISCO
-  def create
-    cluster = params[:cluster]
-    uri = URI.parse(@@disco_ip)
-    request = Net::HTTP::Post.new(uri)
-    request.content_type        = "text/occi"
-    request["Category"]         = 'haas; scheme="http://schemas.cloudcomplab.ch/occi/sm#"; class="kind";'
-    request["X-Tenant-Name"]    = current_user[:tenant]
-    request["X-Region-Name"]    = 'RegionOne'
-    request["X-User-Name"]      = current_user[:username]
-    request["X-Password"]       = current_user[:disco_ip]
-    request["X-Occi-Attribute"] = 'icclab.haas.master.image="'+cluster[:master_image]+'",icclab.haas.slave.image="'+cluster[:slave_image]+'",icclab.haas.master.sshkeyname="discokey",icclab.haas.master.flavor="'+cluster[:master_flavor]+'",icclab.haas.slave.flavor="'+cluster[:slave_flavor]+'",icclab.haas.master.number="'+cluster[:master_num]+'",icclab.haas.slave.number="'+cluster[:slave_num]+'",icclab.haas.master.slaveonmaster="on",icclab.haas.master.withfloatingip="true",icclab.disco.frameworks.spark.included="True",icclab.disco.frameworks.hadoop.included="True",icclab.disco.frameworks.zeppelin.included="True",icclab.disco.frameworks.jupyter.included="True"'
 
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-      http.request(request)
-    end
-
-    if response != 200
-      raise
-    end
-
-    redirect_to root_url
-  end
-
-  # Method to delete chosen cluster
-
-  def delete
-    uuid = params[:uuid]
-    uri  = URI.parse(@@disco_ip+"#{uuid}")
-    request = Net::HTTP::Delete.new(uri)
-    request.content_type     = "text/occi"
-    request["Category"]      = 'haas; scheme="http://schemas.cloudcomplab.ch/occi/sm#"; class="kind";'
-    request["X-Tenant-Name"] = current_user[:tenant]
-    request["X-Region-Name"] = 'RegionOne'
-    request["X-User-Name"]   = current_user[:username]
-    request["X-Password"]    = current_user[:disco_ip]
-
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-      http.request(request)
-    end
-
-    if response.code != 200
-      raise
-    end
-  end
-
-  # Method to get all details of the chosen cluster
-  def details
-    instance   = params[:server]
-    @title     = instance["kind"]["title"]
-    attributes = instance["attributes"]
-    @master_n  = attributes["icclab.haas.master.number"]
-    @slave_n   = attributes["icclab.haas.slave.number"]
-    @info      = attributes["externalIP"]+" <p></p> "+attributes["statusText"]
-    @id        = attributes["occi.core.id"]
-    @id.slice! '/haas/'
-    @uuid      = "delete?uuid="+@id
-
-    respond_to do |format|
-      format.js
-    end
-  end
 
   # Helper methods to get image and flavor names
     def get_image(stack)
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  get    '/debug',   to: 'pages#debug'
-  get    '/details', to: 'pages#details'
-  post   '/delete',  to: 'pages#delete'
-  resources :pages, only:[:create, :delete]
-  resources :users
+  get    '/details', to: 'cluster#show'
+  post   '/delete',  to: 'cluster#destroy'
+  post   '/create',  to: 'cluster#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
