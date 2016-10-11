f925b8100aa1e02a0631011b0590daabf0fc312d
Saken <s.kenzhegulov@mail.ru>
2016-10-11 16:15:45 +0200

    Commit before big change :P

--- app/controllers/cluster_controller.rb
+++ app/controllers/cluster_controller.rb
@@ -1,6 +1,6 @@
 class ClusterController < ApplicationController
   before_action :logged_in_user
-  before_action :correct_user, only: [:destroy, :show]
+  # before_action :correct_user, only: :destroy
 
 # Method to create a cluster on DISCO
   def create
@@ -71,55 +71,6 @@ class ClusterController < ApplicationController
     end
   end
 
-  # Method to update all clusters of current user
-  def update_all
-    user_clusters = current_user.clusters.all
-
-    send_request
-
-    clusters = ''
-    response.header.each_header {|key,value| clusters = value.split(', ') if key=='x-occi-location' }
-
-    clusters.each { |cluster| send_request(cluster) }
-
-    clusters.each do |cluster|
-      response = send_request(cluster, 'json')
-
-      if response.code == "200"
-        disco_cluster = JSON.parse(response.body)
-        uuid = disco_cluster["attributes"]["occi.core.id"]
-        uuid.slice! '/haas/'
-        state = disco_cluster["attributes"]["stack_status"]
-
-        user_cluster = user_clusters.find { |s| s[:uuid] == uuid }
-        if user_cluster
-          user_cluster.update_attribute(:state, state)
-        else
-          attributes = disco_cluster["attributes"]
-          new_cluster = {
-            uuid:          uuid,
-            state:         state,
-            name:          "cluster"+uuid,
-            master_name:   "master"+uuid,
-            slave_name:    "slave"+uuid,
-            master_image:  attributes["icclab.haas.master.image"],
-            master_flavor: attributes["icclab.haas.master.flavor"],
-            slave_image:   attributes["icclab.haas.slave.image"],
-            slave_flavor:  attributes["icclab.haas.slave.flavor"],
-            master_slave:  attributes["icclab.haas.master.slaveonmaster"]=="on" ? true : false
-          }
-          current_user.clusters.build(new_cluster)
-          if instance.save
-            puts 'New cluster added from disco'
-          else
-            raise
-          end
-        end
-      end
-
-    end
-  end
-
   private
     def cluster_params
       params.require(:cluster).permit(:name, :uuid, :state,
@@ -131,7 +82,7 @@ class ClusterController < ApplicationController
     end
 
     def correct_user
-      @cluster = current_user.clusters.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
+      cluster = current_user.clusters.find_by(id: params[:id])
+      redirect_to root_url if cluster.nil?
     end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   before_action :logged_in_user
   before_action :authenticate_to_openstack
+  before_action :update_all
 
   helper_method :get_image, :get_flavor
 
@@ -45,5 +46,55 @@ class PagesController < ApplicationController
       id_m = stack["attributes"]["icclab.haas.master.flavor"]
       id_s = stack["attributes"]["icclab.haas.slave.flavor"]
       return @@openstack.get_flavor(id_m), @@openstack.get_flavor(id_s) if id_s && id_m
+    end
+
+  private
+    # Method to update all clusters of current user
+    def update_all
+      user_clusters = current_user.clusters.all
+
+      response = send_request
+
+      clusters = ''
+      response.header.each_header {|key,value| clusters = value.split(', ') if key=='x-occi-location' }
+
+      clusters.each { |cluster| send_request(cluster) }
+
+      clusters.each do |cluster|
+        response = send_request(cluster, 'json')
+
+        if response.code == "200"
+          disco_cluster = JSON.parse(response.body)
+          uuid = disco_cluster["attributes"]["occi.core.id"]
+          uuid.slice! '/haas/'
+          state = disco_cluster["attributes"]["stack_status"]
+
+          user_cluster = user_clusters.find { |s| s[:uuid] == uuid }
+          if user_cluster
+            user_cluster.update_attribute(:state, state)
+          else
+            attributes = disco_cluster["attributes"]
+            new_cluster = {
+              uuid:          uuid,
+              state:         state,
+              name:          "cluster"+uuid,
+              master_name:   "master"+uuid,
+              slave_name:    "slave"+uuid,
+              master_image:  attributes["icclab.haas.master.image"],
+              master_flavor: attributes["icclab.haas.master.flavor"],
+              slave_image:   attributes["icclab.haas.slave.image"],
+              slave_flavor:  attributes["icclab.haas.slave.flavor"],
+              master_slave:  attributes["icclab.haas.master.slaveonmaster"]=="on" ? true : false
+            }
+            instance = current_user.clusters.build(new_cluster)
+            if instance.save
+              puts 'New cluster added from disco'
+            else
+              raise
+            end
+          end
+        end
+
+      end
     end
 end
