4e912e3ea6ae13a767aae571314edc227ef9d09c
Saken <s.kenzhegulov@mail.ru>
2016-10-19 11:22:11 +0200

    basic login changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,45 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session = params[:session]
-    openstack = OpenStack::Connection.create ({
-        username:   session[:username],
-        api_key:    session[:password],
-        auth_url:   session[:auth_url],
-        authtenant: session[:tenant]
-      })
-    error if !openstack
-
-    user = User.find_by(username: params[:session][:username].downcase)
-    if user
-      if user.authenticate(params[:session][:password])
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to root_path
-      else
-        error
-      end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to root_path
     else
-      user = User.new(session_params.except(:remember_me))
-      user[:disco_ip] = params[:session][:password]
-      if user.save
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to root_path
-      else
-        error
-      end
+      flash.now[:danger] = 'Invalid username/password combination'
+      render 'new'
     end
   end
 
   def destroy
     log_out if logged_in?
     redirect_to login_path
-  end
-
-  def error
-    flash.now[:danger] = 'Invalid username/password combination'
-    render 'new'
   end
 
   private
