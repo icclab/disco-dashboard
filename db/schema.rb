49c6ef6791b6b4b6bccbb19f5daf4eb62fa008d7
Saken <kenz@zhaw.ch>
2016-11-08 17:06:47 +0100

    added assignment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108133339) do
+ActiveRecord::Schema.define(version: 20161108160454) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "cluster_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cluster_id"], name: "index_assignments_on_cluster_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "cluster_frameworks", force: :cascade do |t|
     t.integer  "cluster_id"
