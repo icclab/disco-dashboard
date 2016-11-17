b1cf784b0a2107fd973ee28d2f4d994889e0ff90
Saken <kenz@zhaw.ch>
2016-11-17 16:54:59 +0100

    User model tests were added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "example@user.com", usertype: 1,
+                     password: "password", password_confirmation: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?, @user.errors.full_messages
+  end
+
+  test "username should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "username should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "username should have a maximum length" do
+    @user.email = ("a" * 255) + "@user.com"
+    assert_not @user.valid?
+  end
+
+  test "username should have the right format" do
+    %w[foo@mail.ch bar@gg.ac.kr foobar@gmail.ru foo_bar@zhaw.co FooBar8@mail.com].each do |email|
+      @user.email = email
+      assert @user.valid?, @user.email
+    end
+    %w[foo-bar@user foobar$user.com *bar@mail.co].each do |email|
+      @user.email = email
+      assert_not @user.valid?, @user.email
+    end
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
