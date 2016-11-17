8157ba7933d3e6f22372b123ba009622ca064d48
Saken <kenz@zhaw.ch>
2016-11-17 17:16:30 +0100

    Added some tests on session controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,15 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !cookies['user_id'].empty?
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  def login_as(user, password: "password")
+    post login_url, params: { sessison: { email:    user.email,
+                                          password: password    } }
+  end
 end
