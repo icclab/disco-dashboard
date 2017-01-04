0cd7b2ac3943079ddc83a33b24842982ca789b00
Saken <kenz@zhaw.ch>
2017-01-04 17:45:24 +0100

    All DISCO requests moved to the separate disco_helper

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -5,6 +5,8 @@ class ClustersController < ApplicationController
     is_permitted?("cluster")
   end
 
+  include DiscoHelper
+
   def index
     @clusters = current_user.clusters.all
     @groups = current_user.groups.all
@@ -26,13 +28,10 @@ class ClustersController < ApplicationController
     @infrastructures = current_user.infrastructures.all
     @adapters        = { "Choose" => 0 }
     @infrastructures.each { |inf| @adapters[inf.name] = inf.id } if @infrastructures
+
     frameworks = Framework.all
     infrastructure = Infrastructure.find(params[:cluster][:infrastructure_id])
     cluster = infrastructure.clusters.build(cluster_params)
-    cluster[:group_id] = 0
-
-    Rails.logger.debug "#{cluster.inspect}"
-    Rails.logger.debug "#{cluster.valid?.inspect}"
 
     if cluster.save
       # If new cluster is properly configured then we send request to the DISCO
@@ -128,85 +127,5 @@ class ClustersController < ApplicationController
 
     def value(val)
       val.to_i==1 ? "true" : "false"
-    end
-
-    def create_req(cluster, infrastructure, frameworks)
-      uri     = URI.parse(ENV["disco_ip"])
-      request = Net::HTTP::Post.new(uri)
-
-      request.content_type         = "text/occi"
-      request["Category"]          = 'haas; scheme="http://schemas.cloudcomplab.ch/occi/sm#"; class="kind";'
-      request["X-Tenant-Name"]     = infrastructure[:tenant]
-      request["X-Region-Name"]     = ENV["region"]
-      request["X-User-Name"]       = infrastructure[:username]
-      request["X-Password"]        = cluster[:password]
-
-      master_image = Image.find(cluster[:master_image])
-      request["X-Occi-Attribute"]  = 'icclab.haas.master.image="'+master_image.img_id+'",'
-      slave_image = Image.find(cluster[:slave_image])
-      request["X-Occi-Attribute"] += 'icclab.haas.slave.image="'+slave_image.img_id+'",'
-
-      request["X-Occi-Attribute"] += 'icclab.haas.master.sshkeyname="'+cluster[:keypair]+'",'
-
-      master_flavor = Flavor.find(cluster[:master_flavor])
-      request["X-Occi-Attribute"] += 'icclab.haas.master.flavor="'+master_flavor.fl_id+'",'
-      slave_flavor = Flavor.find(cluster[:slave_flavor])
-      request["X-Occi-Attribute"] += 'icclab.haas.slave.flavor="'+slave_flavor.fl_id+'",'
-
-      request["X-Occi-Attribute"] += 'icclab.haas.master.number="'+cluster[:master_num].to_s+'",'
-      request["X-Occi-Attribute"] += 'icclab.haas.slave.number="'+cluster[:slave_num].to_s+'",'
-
-      request["X-Occi-Attribute"] += 'icclab.haas.master.slaveonmaster="'+value(cluster[:slave_on_master])+'",'
-
-      frameworks.each do |framework|
-        if !framework.name.eql? "HDFS"
-          request["X-Occi-Attribute"] += 'icclab.disco.frameworks.'+framework[:name].downcase
-          request["X-Occi-Attribute"] += '.included="'+value(cluster[framework[:name]])+'",'
-        end
-      end
-
-      request["X-Occi-Attribute"] += 'icclab.haas.master.withfloatingip="true"'
-
-      Rails.logger.debug {"Cluster attributes: #{request["X-Occi-Attribute"].inspect}"}
-
-      response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-        http.request(request)
-      end
-
-      response
-    end
-
-    def delete_req(infrastructure, password, uuid)
-      uri  = URI.parse(ENV["disco_ip"]+uuid)
-
-      request = Net::HTTP::Delete.new(uri)
-      request.content_type     = "text/occi"
-      request["Category"]      = 'haas; scheme="http://schemas.cloudcomplab.ch/occi/sm#"; class="kind";'
-      request["X-Tenant-Name"] = infrastructure[:tenant]
-      request["X-Region-Name"] = ENV["region"]
-      request["X-User-Name"]   = infrastructure[:username]
-      request["X-Password"]    = password
-
-      response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-        http.request(request)
-      end
-
-      response
-    end
-
-    def send_request(cluster, infrastructure, uuid = '')
-      url = ENV["disco_ip"]+uuid
-      uri     = URI.parse(url)
-      request = Net::HTTP::Get.new(uri)
-      request["X-User-Name"]   = infrastructure[:username]
-      request["X-Password"]    = cluster[:password]
-      request["X-Tenant-Name"] = infrastructure[:tenant]
-      request["X-Region-Name"] = ENV["region"]
-      request["Accept"]        = "text/occi"
-      response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-        http.request(request)
-      end
-
-      response
     end
 end
---
+++ app/helpers/disco_helper.rb
@@ -0,0 +1,81 @@
+module DiscoHelper
+  def create_req(cluster, infrastructure, frameworks)
+    uri     = URI.parse(ENV["disco_ip"])
+    request = Net::HTTP::Post.new(uri)
+
+    request.content_type         = "text/occi"
+    request["Category"]          = 'haas; scheme="http://schemas.cloudcomplab.ch/occi/sm#"; class="kind";'
+    request["X-Tenant-Name"]     = infrastructure[:tenant]
+    request["X-Region-Name"]     = ENV["region"]
+    request["X-User-Name"]       = infrastructure[:username]
+    request["X-Password"]        = cluster[:password]
+
+    master_image = Image.find(cluster[:master_image])
+    request["X-Occi-Attribute"]  = 'icclab.haas.master.image="'+master_image.img_id+'",'
+    slave_image = Image.find(cluster[:slave_image])
+    request["X-Occi-Attribute"] += 'icclab.haas.slave.image="'+slave_image.img_id+'",'
+
+    request["X-Occi-Attribute"] += 'icclab.haas.master.sshkeyname="'+cluster[:keypair]+'",'
+
+    master_flavor = Flavor.find(cluster[:master_flavor])
+    request["X-Occi-Attribute"] += 'icclab.haas.master.flavor="'+master_flavor.fl_id+'",'
+    slave_flavor = Flavor.find(cluster[:slave_flavor])
+    request["X-Occi-Attribute"] += 'icclab.haas.slave.flavor="'+slave_flavor.fl_id+'",'
+
+    request["X-Occi-Attribute"] += 'icclab.haas.master.number="'+cluster[:master_num].to_s+'",'
+    request["X-Occi-Attribute"] += 'icclab.haas.slave.number="'+cluster[:slave_num].to_s+'",'
+
+    request["X-Occi-Attribute"] += 'icclab.haas.master.slaveonmaster="'+value(cluster[:slave_on_master])+'",'
+
+    frameworks.each do |framework|
+      if !framework.name.eql? "HDFS"
+        request["X-Occi-Attribute"] += 'icclab.disco.frameworks.'+framework[:name].downcase
+        request["X-Occi-Attribute"] += '.included="'+value(cluster[framework[:name]])+'",'
+      end
+    end
+
+    request["X-Occi-Attribute"] += 'icclab.haas.master.withfloatingip="true"'
+
+    Rails.logger.debug {"Cluster attributes: #{request["X-Occi-Attribute"].inspect}"}
+
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(request)
+    end
+
+    response
+  end
+
+  def delete_req(infrastructure, password, uuid)
+    uri  = URI.parse(ENV["disco_ip"]+uuid)
+
+    request = Net::HTTP::Delete.new(uri)
+    request.content_type     = "text/occi"
+    request["Category"]      = 'haas; scheme="http://schemas.cloudcomplab.ch/occi/sm#"; class="kind";'
+    request["X-Tenant-Name"] = infrastructure[:tenant]
+    request["X-Region-Name"] = ENV["region"]
+    request["X-User-Name"]   = infrastructure[:username]
+    request["X-Password"]    = password
+
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(request)
+    end
+
+    response
+  end
+
+  def send_request(cluster, infrastructure, uuid = '')
+    url = ENV["disco_ip"]+uuid
+    uri     = URI.parse(url)
+    request = Net::HTTP::Get.new(uri)
+    request["X-User-Name"]   = infrastructure[:username]
+    request["X-Password"]    = cluster[:password]
+    request["X-Tenant-Name"] = infrastructure[:tenant]
+    request["X-Region-Name"] = ENV["region"]
+    request["Accept"]        = "text/occi"
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(request)
+    end
+
+    response
+  end
+end
