62f95d1875a7647590970de7b27c65e2a676f3d9
Balazs Meszaros <mesz@zhaw.ch>
2017-02-28 08:53:31 +0100

    - framework ports will only be checked now if the IP is not 0

--- app/helpers/cluster_helper.rb
+++ app/helpers/cluster_helper.rb
@@ -30,9 +30,14 @@ module ClusterHelper
     uri = URI.parse(url)
     response = nil
     begin
-      Net::HTTP.start(uri.host, uri.port) { |http|
-        response = http.head(uri.path.size > 0 ? uri.path : "/")
-      }
+      # uri.host is 0 as long as no other value has been delivered by DISCO
+      if uri.host != "0"
+        Net::HTTP.start(uri.host, uri.port) { |http|
+          response = http.head(uri.path.size > 0 ? uri.path : "/")
+        }
+      else
+        return "danger"
+      end
     rescue StandardError
       return "danger"
     end
