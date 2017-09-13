f4300c2a1ac80baafd1b3a52aa933aeda2957a0a
Balazs Meszaros <Pentadactylus@users.noreply.github.com>
2017-09-13 16:18:37 +0200

    import GPG key for installing rvm

--- setup.sh
+++ setup.sh
@@ -34,6 +34,7 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y redis-server
 
 # Downloading and installing RVM and Ruby 2.3.1
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -L https://get.rvm.io | bash -s stable --ruby=2.3.1
 source /home/$USER/.rvm/scripts/rvm
 # Configuring and updating Gem Manager
