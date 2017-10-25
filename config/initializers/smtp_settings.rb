710eba6b6b9c744b3f86110ba5794a99569c8022
Balazs Meszaros <mesz@zhaw.ch>
2017-10-25 09:41:02 +0200

    added automatic handling of SMTP server without authentication (i.e. no given username, password and domain)

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,10 +1,20 @@
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => ENV['smtp_enable_starttls_auto'],
-    :address => ENV['smtp_server'],
-    :port => ENV['smtp_port'].to_i,
-    :authentication => :plain,
-    :domain => ENV['smtp_domain'],
-    :user_name => ENV['smtp_username'],
-    :password => ENV['smtp_password']
-}
+
+if (ENV['smtp_username'].nil? || ENV['smtp_username'].empty?) && (ENV['smtp_password'].nil? || ENV['smtp_password'].empty?) && (ENV['domain'].nil? || ENV['domain'].empty?)
+  ActionMailer::Base.smtp_settings = {
+      :enable_starttls_auto => ENV['smtp_enable_starttls_auto'],
+      :address => ENV['smtp_server'],
+      :port => ENV['smtp_port'].to_i,
+      :authentication => :plain
+  }
+else
+  ActionMailer::Base.smtp_settings = {
+      :enable_starttls_auto => ENV['smtp_enable_starttls_auto'],
+      :address => ENV['smtp_server'],
+      :port => ENV['smtp_port'].to_i,
+      :authentication => :plain,
+      :domain => ENV['smtp_domain'],
+      :user_name => ENV['smtp_username'],
+      :password => ENV['smtp_password']
+  }
+end
